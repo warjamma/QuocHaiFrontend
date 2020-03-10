c441ae2313249f31093372a9c2d99bbfc5ed0af4
apolo <apologize216@gmail.com>
2020-03-11 01:15:47 +0700

    update

--- .umirc.js
+++ .umirc.js
@@ -1,15 +1,49 @@
 
 // ref: https://umijs.org/config/
+
 export default {
   treeShaking: true,
   routes: [
     {
-      path: '/',
-      component: '../layouts/index',
+      path: '/dashboard',
+      component: '../layouts/DashboardLayout/index',
       routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+        {
+          path: '/dashboard',
+          component: '../pages/Dashboard/index'
+        },
+      ],
+    },
+    {
+      path: '/register',
+      component: '../layouts/AuthenticateLayout/index',
+      routes: [
+        {
+          path: '/register',
+          component: '../pages/Register/index',
+        },
+      ],
+    },
+    {
+      path: '/',
+      component: '../layouts/AuthenticateLayout/index',
+      routes: [
+        {
+          path: '/',
+          component: '../pages/Login/index',
+        },
+      ],
+    },
+    // {
+    //   path: '/',
+    //   component: '../layouts/UserLayout/index',
+    //   routes: [
+    //     {
+    //       path: '/',
+    //       component: '../pages/Dashboard/index',
+    //     },
+    //   ],
+    // },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
