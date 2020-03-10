300ca5d44f295b307ddd31098895e39e2c047b52
apolo <apologize216@gmail.com>
2020-03-11 01:19:41 +0700

    import ant

--- .umirc.js
+++ .umirc.js
@@ -48,7 +48,7 @@ export default {
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
-      antd: false,
+      antd: true,
       dva: false,
       dynamicImport: false,
       title: 'frontend',
