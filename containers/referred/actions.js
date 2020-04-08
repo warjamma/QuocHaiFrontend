2c4e30ee3f78b523eaaa9d1753c6df54df9b4375
lgold141 <lgold141@gmail.com>
2020-04-09 00:06:03 +0700

    Connect API job detail upload cv

--- containers/referred/actions.js
+++ containers/referred/actions.js
@@ -8,7 +8,20 @@ export function getListJob(params) {
       dispatch({ type: "GET_LIST_JOB_SUCCESS", data: data });
     } catch (error) {
       const { data } = error.response
-      return dispatch({ type: "GET_LIST_JOB_SUCCESS_FAILURE", error: data.message });
+      return dispatch({ type: "GET_LIST_JOB_FAILURE", error: data.message });
+    }
+  };
+}
+
+export function getJobById(params) {
+  //console.log('params', params);
+  return async dispatch => {
+    try {
+      const { data } = await api.sendRequestWithToken('get', `/jobs/${params.id}`);
+      dispatch({ type: "GET_JOB_BY_ID_SUCCESS", data: data });
+    } catch (error) {
+      const { data } = error.response
+      return dispatch({ type: "GET_JOB_BY_ID_FAILURE", error: data.message });
     }
   };
 }
@@ -21,6 +34,18 @@ export function getListReferred(params) {
     } catch (error) {
       const { data } = error.response
       return dispatch({ type: "GET_LIST_REFERRED_FAILURE", error: data.message });
+    }
+  };
+}
+
+export function createCandidate(payload) {
+  return async dispatch => {
+    try {
+      await api.sendRequestWithToken('post', '/candidates', null, null, payload);
+      return { status: true }
+    } catch (error) {
+      const { data } = error.response
+      return { status: false, error: data.message };
     }
   };
 }
