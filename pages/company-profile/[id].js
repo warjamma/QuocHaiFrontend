dcbae5acad79715b0cdda73e6f930311277a9b79
whis-frontend <lgold141@gmail.com>
2020-04-23 16:36:45 +0700

    update UI my profile

--- pages/company-profile/[id].js
+++ pages/company-profile/[id].js
@@ -3,9 +3,8 @@ import { connect } from 'react-redux'
 import Router, { useRouter } from 'next/router';
 import { Table, Tag, Button, Form, Row, Col, Input, Select, Typography, Card, Alert, Text } from 'antd';
 import { getJobById } from '../../containers/referred/actions';
-import Link from 'next/link'
-import axios from 'axios';
 import { get } from 'lodash';
+import './styles.scss'
 import {
     DollarOutlined,
     RightOutlined,
@@ -40,8 +39,8 @@ function jobDetail(props) {
 
 
     return (
-        <div style={{ padding: 30 }}>
-            <Title level={2}>Company Profile</Title>
+        <div className="company-profile" style={{ padding: 30 }}>
+             <div className="header" style={{ backgroud: '#fff', fontWeight: 'bold' }}>Company profile</div>
             <Row gutter={[16, 16]}>
                 <Col span={24} >
                     <div style={{ background: 'white' }}>
@@ -61,99 +60,7 @@ function jobDetail(props) {
                         </div>
                     </div>
                 </Col>
-                {/* <Col span={16} >
-                    <Card className="titileJob">
-                        <Title level={3}>{get(referred, 'job_detail.data.job.job_title')}</Title>
-                        <Button style={{ marginRight: 5 }}>{get(referred, 'job_detail.data.job.job_role')}</Button>
-                        <Button>Spring</Button>
-                        <div style={{ color: '#68ba50' }}><DollarOutlined /> You'll love it</div>
-                        <div ><RightOutlined /> FPT Software Đường D1 Phường Tân Phú , District 9, Ho Chi Minh</div>
-                        <a style={{ color: '#68ba50', fontSize: '13px', paddingLeft: 10 }}>See map<CaretRightOutlined /></a>
-                        <div ><RightOutlined /> Indonesia <a style={{ color: '#68ba50', fontSize: '13px' }}>See map<CaretRightOutlined /></a></div>
-                        <div ><CalendarOutlined /> 4 days ago</div>
-                        <Button style={get(profile, 'data.employer.id') ? ({ visibility: "hidden" }) : ({ float: 'left', marginRight: 5, width: '40%', display: 'block' })} type="primary" onClick={() => Router.push(`/referrer/upload-cv/${id}`)} block>Giới thiệu ứng viên</Button>
-                        <Button style={{ float: 'left', width: '40%' }}  disabled={get(referred, 'job_detail.data.job.jd_files') == ''?(true):( false)  } type="primary" block><a href={get(referred, 'job_detail.data.job.jd_files')} download>Dowload JD</a></Button>
-                    </Card>
-                    <Card className="contentJob" >
-                        <Title level={3}>Top 3 Reasons To Join Us</Title>
-                        <ul>
-                            <li style={{ fontWeight: 'bold' }} level={4}>Global Exposure </li>
-                            <li style={{ fontWeight: 'bold' }} level={4}>Global Exposure</li>
-                            <li style={{ fontWeight: 'bold' }} level={4}>Global Exposure</li>
-                        </ul>
-                        <Title level={3}>The Job</Title>
-                        <ul>
-                            <li style={{ marginLeft: 10 }} level={4}>Study customer’s requirement and framework, create design documents, implementation and coding of solutions.</li>
-                            <li style={{ marginLeft: 10 }} level={4}>Generate unit tests to ensure the products are being tested thoroughly to produce quality products</li>
-                            <li style={{ marginLeft: 10 }} level={4}>Supporting quality assurance in testing software products.
-                        Interacting with customers and partners about product explanation, implementation and support</li>
-                        </ul>
-
-                        <Title level={3}>Your Skills and Experience</Title>
-                        <ul>
-                            <li style={{ marginLeft: 10 }} level={4}>Study customer’s requirement and framework, create design documents, implementation and coding of solutions.</li>
-                            <li style={{ marginLeft: 10 }} level={4}>Generate unit tests to ensure the products are being tested thoroughly to produce quality products</li>
-                            <li style={{ marginLeft: 10 }} level={4}>Supporting quality assurance in testing software products.
-                        Interacting with customers and partners about product explanation, implementation and support</li>
-                        </ul>
-
-                        <Title level={3}>Why You'll Love Working Here</Title>
-                        <ul>
-                            <li style={{ marginLeft: 10 }} level={4}>Salary negotiation.</li>
-                            <li style={{ marginLeft: 10 }} level={4}>All benefits based on Labor Law & FPT Software’s policy.</li>
-                            <li style={{ marginLeft: 10 }} level={4}>English-friendly, international working environment</li>
-                            <li style={{ marginLeft: 10 }} level={4}>Onsite opportunities</li>
-                        </ul>
-                        <Button style={get(profile, 'data.employer.id') ? ({ visibility: "hidden" }) : ({ float: 'left', marginRight: 5, width: '40%', display: 'block' })} type="primary" onClick={() => Router.push(`/referrer/upload-cv/${id}`)} block>Giới thiệu ứng viên</Button>
-                        <Button style={{ float: 'left', width: '40%' }} type="primary" block>Dowload JD</Button>
-                    </Card>
-                </Col>
-            */}
             </Row>
-
-            {/* <Card className="involveJob" >
-                <Title level={3}>More Jobs for You</Title>
-                <Card className="moreJobs">
-                    <Row gutter={[16, 16]} className="oneRow">
-                        <Col span={6} >
-                            <img alt="example" src="https://cdn.itviec.com/employers/dxc-vietnam/logo/s65/U7GERVChzwPaBYXec1bQgPmP/DXC%20logo-icon.png" />
-                        </Col>
-                        <Col span={18} >
-                            <Row>
-                                <Col span={18} >
-                                    <Title level={3}>Why You'll Love Working Here</Title>
-                                    <div style={{ color: '#68ba50' }}><DollarOutlined /> You'll love it</div>
-                                </Col>
-                                <Col span={6} >
-                                    <div style={{ float: 'right', background: '#ff9e34', color: 'white', fontSize: '8', padding: 3 }}>Ant Design</div><br />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col span={18} >
-                                    <div>Role: As a COBOL Software Engineer, you will collaborate proactively with functional analysts to transla
-                                            te business and integration requirements into...</div>
-                                </Col>
-                                <Col span={6} >
-                                    <div style={{ float: 'right' }}>Hồ Chí Minh</div><br />
-                                    <div style={{ float: 'right' }}>Tân Bình</div>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col span={18} >
-                                    <Button style={{ marginRight: 5 }}>J2EE</Button>
-                                    <Button style={{ marginRight: 5 }}>Java</Button>
-                                    <Button>Spring</Button>
-                                </Col>
-                                <Col span={6} >
-                                    <div style={{ float: 'right' }}>7 minutes ago</div><br />
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Card>
-
-            </Card>
-         */}
         </div>
     );
 }
