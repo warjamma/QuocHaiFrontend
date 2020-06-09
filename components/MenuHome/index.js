d3867c60e5d9bc64cc19b20496c64bb4ae135d74
lgold141 <lgold141@gmail.com>
2020-06-09 16:25:39 +0700

    update get list job update UI home

--- components/MenuHome/index.js
+++ components/MenuHome/index.js
@@ -1,33 +1,51 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
+import Router from 'next/router';
 import { MenuOutlined } from '@ant-design/icons';
 import './styles.scss';
 
 function HeaderHome() {
-  const [isChoosen,setChoosen]=useState(false);
+  const [isChoosen, setChoosen] = useState(false);
   const myFunction = () => {
     setChoosen(!isChoosen);
   };
 
- 
   return (
     <div className="menu-top">
       <div className="container full-wight">
-      {/* <a href="#home" className="logo" >
-        <img className="name-logo" src="https://www.rockship.co/images/rs-logo-text.png" alt="#"/>
-      </a> */}
-      <div className={classNames('topnav',{ 'responsive': isChoosen },{ 'topnav': !isChoosen })} id="myTopnav">
-      <div className="child">
-      <a href="#news" className="active tag">Đăng nhập</a>
-      <a href="#contact" className="  tag">Ứng tuyển</a>
-      <a href="#about" className="  tag">It companies  </a>
-      <a href="#about" className="  tag">All Jobs  </a>
+        <div
+          className={classNames(
+            'topnav',
+            { responsive: isChoosen },
+            { topnav: !isChoosen }
+          )}
+          id="myTopnav"
+        >
+          <li className="logo" role="presentation"   onClick={() => Router.push(`/`)}>
+            <img
+              className="name-logo"
+              src="https://www.rockship.co/images/rs-logo-text.png"
+              alt="#"
+            />
+          </li>
+          <div className="child">
+            <div className="down">
+              <li role="presentation" onClick={() => Router.push(`/login`)} className="active tag">Đăng nhập</li>
+              <li className="  tag" role="presentation"   onClick={() => Router.push(`/upload-cv-home`)}>Ứng tuyển</li>
+              <li className="  tag" role="presentation"   onClick={() => Router.push(`/upload-home`)}>It companies </li>
+              <li className="  tag" role="presentation"   onClick={() => Router.push(`/upload-home`)}>All Jobs </li>
+            </div>
+          </div>
+          <li className="icon">
+            {' '}
+            <a href="avascript:void(0);" onClick={() => myFunction()}>
+              <div>
+                <MenuOutlined />
+              </div>
+            </a>
+          </li>
+        </div>
       </div>
-      <a href="avascript:void(0);" className="icon" onClick={()=>myFunction()}>
-        <div><MenuOutlined /></div>
-      </a>
-    </div>
-    </div>
     </div>
   );
 }
--- containers/job/actions.js
+++ containers/job/actions.js
@@ -36,3 +36,16 @@ export function getAllJobType(params) {
     }
   };
 }
+
+export function getListJob(params) {
+  return async dispatch => {
+    try {
+      dispatch({ type: "GET_LIST_REQUEST" });
+      const { data } = await api.sendRequest('get', `/jobs?${qs.stringify(params)}`,null,{"Authorization":"JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjgyNDhjNWM0LTUwNzgtNDI2ZS05NmQ3LWY0Y2UwMGRhMmY5MyIsImlhdCI6MTU5MTYxMDY4Mywicm9sZSI6ImVtcGxveWVyIn0.fTpZvcvQgFFfsEdfXBe_qTSjvP1eqnyXBdNl_GBwl9g"});
+      return dispatch({ type: "GET_LIST_JOB_SUCCESS", data });
+    } catch (error) {
+      const { data } = error.response;
+      return dispatch({ type: "GET_LIST_JOB_FAILURE", error: data.message });
+    }
+  };
+}
